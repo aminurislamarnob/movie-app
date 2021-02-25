11afa3ee0573524abc21743118e68e42f53cd504
Md Aminur Islam <eng.aminurislam@gmail.com>
2021-02-25 12:05:26 +0600

    home and details page desing

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+    "use strict";
+    
+    //Main Slider
+    
+    $('#main-slider').owlCarousel({
+        margin:10,
+        nav: false,
+        items: 1,
+        dots: true,
+        margin: 0,
+        autoplay: true,
+        loop: true
+    })
+    
+}(jQuery));	
